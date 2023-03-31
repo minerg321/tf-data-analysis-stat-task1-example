c0a29ed32139f5865f80c765aa7015f393dbd9e7
Mihail Lavruhin <36154473+minerg321@users.noreply.github.com>
2023-03-31 23:02:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
+from scipy.optimize import minimize
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 575072396 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+   # Определение функции правдоподобия модели
+    def likelihood_func(mu: float, x: np.array) -> float:
+        n = len(x)
+        return -n * np.log(mu) - np.sum(x) / mu
+
+    # Максимизация функции правдоподобия
+    res = minimize(likelihood_func, x0=1, args=(x,), method='Nelder-Mead')
+    
+    # Возвращение оценки параметра модели
+    return res.x[0]
